3a5b35765772479b3dc702b7c7f145060d5f99ce
YedoCosta <seu email>
2022-08-01 11:50:53 -0300

    cria tela de conexao

---
+++ ModuloAutenticacao.Classes/Conexao.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Common;
+
+namespace ModuloAutenticacao.Classes
+{
+    internal class Conexao
+    {
+        public static DbConnection _conn;
+
+        public int numero;
+        public double PI = 34.67;
+        public int idade = 34;
+
+    }
+}
